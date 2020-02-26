d4f33b140d28d670766e6d7ba8f5d44f2b17d9f2
FAONbr <faonbr@gmail.com>
2020-02-25 23:00:22 -0300

    a

--- app/controllers/messages.controller.js
+++ app/controllers/messages.controller.js
@@ -25,7 +25,7 @@ exports.listAll = function(req, res){
   logger.log('-----------------');
   logger.log('Entering messages listAll function.');
   var params = {
-    ProjectionExpression: "from-id, to-id, message, sent-date",
+    ProjectionExpression: "fromid, toid, message, sent-date",
     TableName: TABLE_NAME
   };
 
--- app/model/messages.model.js
+++ app/model/messages.model.js
@@ -1,24 +1,24 @@
 {
-  "from-id": "1",
+  "fromid": "1",
   "message": "Olá Mamae. Aqui é o papai.",
-  "sent-date": 1582680700506,
-  "to-id": "2"
+  "sentdate": 1582680700506,
+  "toid": "2"
 },
 {
-  "from-id": "1",
+  "fromid": "1",
   "message": "Carol, vai estudar.",
-  "sent-date": 1582680700507,
-  "to-id": "3"
+  "sentdate": 1582680700507,
+  "toid": "3"
 },
 {
-  "from-id": "1",
+  "fromid": "1",
   "message": "Larissa, vai arrumar o quarto.",
-  "sent-date": 1582680700510,
-  "to-id": "4"
+  "sentdate": 1582680700510,
+  "toid": "4"
 },
 {
-  "from-id": "2",
+  "fromid": "2",
   "message": "Olá papai. Aqui é a mamae.",
-  "sent-date": 1582680700508,
-  "to-id": "1"
+  "sentdate": 1582680700508,
+  "toid": "1"
 }
